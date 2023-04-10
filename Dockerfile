7a1a4a4429a4c325638e34e1f51866a785d996a7
Logesh <logeshkrishna08@gmail.com>
2023-04-10 09:43:06 +0530

    added .env

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM python:3
+
+
+WORKDIR /srv
+
+
+RUN apt-get -y update
+RUN pip install --upgrade pip
+RUN apt-get install zip -y
+RUN apt-get install unzip -y
+
+
+
+
+# Install chromedriver
+RUN wget -N https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip -P ~/
+RUN unzip ~/chromedriver_linux64.zip -d ~/
+RUN rm ~/chromedriver_linux64.zip
+RUN mv -f ~/chromedriver /usr/local/bin/chromedriver
+RUN chown root:root /usr/local/bin/chromedriver
+RUN chmod 0755 /usr/local/bin/chromedriver
+
+
+# Install chrome broswer
+RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+RUN apt-get -y update
+RUN apt-get -y install google-chrome-stable
+
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+ADD . /srv
+
+CMD ["python", "-u", "app.py"]
---
+++ app.py
@@ -0,0 +1,222 @@
+import sys,os,time,json,argparse,random
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+import gzip
+from threading import Thread,current_thread
+from decouple import config
+
+
+MAX_THREADS = 1
+RANGE_SET = 50
+
+jwt_value = config('jwt_value')
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("--mute-audio")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('--no-sandbox')
+
+chrome_options.add_argument("--disable-dev-shm-usage")
+driver_path = '/usr/local/bin/chromedriver'
+
+service = ChromeService(executable_path=driver_path)
+colors = ["\033[91m","\033[92m","\033[95m","\033[96m","\033[93m"]
+totalXP = totalFixes = 0
+
+def getThreadId():
+    return 2
+
+def getAndAddTotalXp():
+    global totalXP
+    totalXP+=10
+    return totalXP
+
+def getTotalXP():
+    global totalXP
+    return totalXP
+
+def getTotalFixes():
+    global totalFixes
+    totalFixes+=1
+    return totalFixes  
+
+def toogleGear(driver):
+    gear = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '._2l-C-._2kfEr._1nlVc._2fOC9.UCrz7.t5wFJ'))
+    )
+    gear.click()
+    switch = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.eP5n4.BLCtW'))
+    )
+    switch.click()
+    done_button = driver.find_element(By.CSS_SELECTOR, '._3HhhB._2NolF._275sd._1ZefG._2zNWD')
+    done_button.click()
+
+def solve(driver,challanges):
+    for challange in challanges:
+        
+        type = challange.get('challengeGeneratorIdentifier').get('specificType')
+        if type=="tap" or type=="reverse_tap" or type=="listen_tap":
+            answers = challange.get('correctTokens')
+            options = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located(
+                    (By.CSS_SELECTOR, '[data-test="word-bank"] > div > button'))
+            )
+            for answer in answers:
+                for option in options:
+                    if option.text==answer:
+                        option.click()
+                        options.remove(option)
+                        time.sleep(.1)
+                        break
+        elif type == 'assist':
+            answers = challange.get('choices')
+            options = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located(
+                    (By.CSS_SELECTOR, 'div._1WbXO._9qT-e.MNYeE._2Nv1I > div > div'))
+            )
+            for option in options:
+                if option.text==answers[challange.get('correctIndex')]:
+                    option.click()
+                    time.sleep(.1)
+                    break
+        elif type == 'form':
+            answers = challange.get('choices')
+            options = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located(
+                    (By.CSS_SELECTOR, 'div._35Xo_._9qT-e._3hkYG._2Nv1I > div > div'))
+            )
+            for option in options:
+                if option.text==answers[challange.get('correctIndex')]:
+                    option.click()
+                    time.sleep(.1)
+                    break
+        elif type=='reverse_translate' or type == 'translate':
+            answer = challange.get('correctSolutions')[0]
+            text_area = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, '[data-test="challenge-translate-input"]'))
+            )
+            text_area.send_keys(answer)
+            time.sleep(.1)
+        else:
+            print(type)
+        next_btn = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, '[aria-disabled="false"][data-test="player-next"]'))
+        )       
+        next_btn.click()
+        next_btn = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, '[aria-disabled="false"][data-test="player-next"]'))
+        )       
+        next_btn.click()
+        try:
+            time.sleep(1)
+            next_btn = WebDriverWait(driver, 2).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, '[aria-disabled="false"][data-test="player-next"]'))
+            )       
+            next_btn.click()
+        except:
+            pass
+    print("{}{} {} +{}xp\033[00m".format(colors[getThreadId()],current_thread().name,'Completed 🔥',getAndAddTotalXp()))  
+    try:
+        again_btn = WebDriverWait(driver, 60).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, '[data-test="player-practice-again"]'))
+            )
+        again_btn.click()
+        return
+    except:
+        raise WebDriverException("Mean it' stuck")
+
+def getDriver():
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
+
+def getPractice(driver):
+    try:
+        driver.get("https://duolingo.com/")
+        driver.add_cookie({'domain':'.duolingo.com','name':'jwt_token','value': jwt_value ,'path':'/'})
+        driver.get("https://duolingo.com/practice")
+        toogleGear(driver)
+        return True
+    except:
+        print("{}Page Not Loaded, Fixing...{}\033[00m".format(colors[getThreadId()],getTotalFixes()))
+        try: 
+            driver.close()
+        except: 
+            pass
+        del driver
+        return False
+
+def core():
+    driver = getDriver()
+    challanges = None
+    if not getPractice(driver): return
+    for _ in range(RANGE_SET):
+        for request in driver.requests:
+            if request.response and request.url=='https://www.duolingo.com/2017-06-30/sessions':
+                decompressed_data = gzip.decompress(request.response.body)
+                utf_8_data = str(decompressed_data,'utf-8')
+                session = json.loads(utf_8_data)
+                challanges = session.get('challenges')
+                if 'adaptiveChallenges' in session.keys():
+                    try:
+                        challanges[15] = session.get('adaptiveChallenges')[1]
+                        challanges[14] = session.get('adaptiveChallenges')[0]
+                    except:
+                        challanges[15] = session.get('adaptiveChallenges')[0]
+                break
+        if not challanges:
+            time.sleep(5)
+        else:
+            del driver.requests
+            try:
+                solve(driver,challanges)
+                challanges = None
+            except WebDriverException:
+                print("{}Fixing...{}\033[00m".format(colors[getThreadId()],getTotalFixes()))
+                try: 
+                    driver.close()
+                except: 
+                    pass
+                del driver
+                return
+    return
+    
+def startThreadMonitor(threads):
+    while True:
+        try:
+            for i in range(len(threads)):
+                thread = threads[i]
+                if not thread.is_alive():
+                    new_thread = Thread(target=core,name=thread.name)
+                    new_thread.start()
+                    threads[i] = new_thread
+                    del thread
+            time.sleep(10)
+        except KeyboardInterrupt:
+            return
+        except:
+            print('Error Occured in Handeler')
+
+            
+
+if __name__ == "__main__":
+    namedThreads = [Thread(name="Duohacker") for i in range(MAX_THREADS)]
+    startThreadMonitor(namedThreads)
